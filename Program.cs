8ccf00a94ddbe455987dcec224be356c1919adf2
Sarah Dennis <sarah.dennis@mymail.champlain.edu>
2020-12-14 13:22:35 -0500

    Starting files uploaded

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RogueGamePractice
+{
+    class Rogue
+    {
+
+        //Main function
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        
+    }
+}
